5e0db37403f39dab00d1f219ecc742cf6beac75e
ゆめ <djym0716@gmail.com>
2017-11-27 13:11:14 +0900

    Implemente post funcition

--- app/controllers/passes_controller.rb
+++ app/controllers/passes_controller.rb
@@ -1,14 +1,29 @@
 class PassesController < ApplicationController
+
+  def index
+    @passes=Pass.all
+  end
+
   def new
+    @pass=Pass.new
+
   end
 
   def create
 
-#ここに保存するための記述をする
+
+Pass.create(pass_params)#ここに保存するための記述をする
 
 
 
 
-  redirect_to :root
+  redirect_to action: 'index'
   end
+
+private
+
+  def pass_params
+    params.require(:pass).permit(:title,:content,:price)
+  end
+
 end

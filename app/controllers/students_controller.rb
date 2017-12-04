dcf2165872b4d8becf1a87b6e964c6c3e4ef4d0c
ゆめ <djym0716@gmail.com>
2017-12-04 16:07:10 +0900

    登録情報を増やした

---
+++ app/controllers/students_controller.rb
@@ -0,0 +1,7 @@
+class StudentsController < ApplicationController
+  def edit
+  end
+
+  def update
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
+
   devise_for :salons
   devise_for :students
 root to: 'tops#index'
 resources :passes
+resources :students, only: [:edit, :update]
+resources :salons, only: [:edit, :update]
 end
